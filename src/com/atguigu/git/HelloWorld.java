fec03dd17db7630d105930a7d5a9ca526917a29d
dawangNie <446275769@qq.com>
2016-05-28 00:05:13 +0800

    Update HelloWorld.java

--- src/com/atguigu/git/HelloWorld.java
+++ src/com/atguigu/git/HelloWorld.java
@@ -4,6 +4,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("aaaaaaaa");
 		System.out.println("bbbbbbbbbbbbbbbb");
-		System.out.println("cccccccccc");
+		System.out.println("ccccc");
 	}
 }
